991748092c9251a20fb2a8df5321ae834568267b
PatrickJS <github@patrickjs.com>
2024-02-13 23:37:54 -0800

    chore: fake server component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,48 @@
-import { component$, useSignal, useTask$ } from '@builder.io/qwik';
-import { isServer } from '@builder.io/qwik/build';
-import { DocumentHead, server$ } from '@builder.io/qwik-city';
+import { JSXOutput, componentQrl } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
 
-import * as fs from "node:fs";
+import { promises as fs } from "node:fs";
 
-export default component$(() => {
-  let file = "";
+// fake asf server component don't actually use this
+export function serverComponentQrl(qrl: () => Promise<JSXOutput>) {
+  // @ts-ignore
+  return componentQrl(qrl);
+}
+export const serverComponent$ = serverComponentQrl;
 
-  if (isServer) {
-    const filename = new URL(import.meta.url).pathname;
-    file = fs.readFileSync(filename, "utf-8");
-  }
+export default serverComponent$(async function testFn() {
+  const filename = new URL(import.meta.url).pathname;
+  let file = await fs.readFile(filename, "utf-8");
 
   return (
-    <>
+    <div id="home">
+      <h1>Welcome to Qwik</h1>
+      <button
+        onClick$={async () => {
+          // client router get latest from server router
+          // location.reload();
+
+          const html = await fetch(location.href).then((res) => res.text());
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+          document.querySelector("#home")!.innerHTML =
+            doc.querySelector("#home")!.innerHTML;
+        }}
+      >
+        update
+      </button>
+      <pre>{Math.random()}</pre>
       <pre>{file}</pre>
-    </>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
